4008b7200aeb9ee808840f4de00b046a3c525844
Artur Galstyan <artur.galstyan@pharos-labs.com>
2025-02-01 15:58:52 +0100

    more keepalive timeout

--- handlers/sse.go
+++ handlers/sse.go
@@ -67,6 +67,7 @@ func CreateSSEHandler(eventSender *events.EventSender, db *gorm.DB) http.Handler
 		w.Header().Set("Cache-Control", "no-cache")
 		w.Header().Set("Connection", "keep-alive")
 		w.Header().Set("X-Accel-Buffering", "no")
+		w.Header().Set("Keep-Alive", "timeout=600, max=1000")
 
 		ticker := time.NewTicker(time.Second)
 		defer ticker.Stop()
